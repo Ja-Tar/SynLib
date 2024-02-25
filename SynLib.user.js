f8e2a942f0534baf87183de67c520355a5120580
Ja Tar <tarnogorskijan@gmail.com>
2024-02-25 17:32:01 +0100

    no i teraz to ja rozumiem

--- SynLib.user.js
+++ SynLib.user.js
@@ -2,21 +2,26 @@
 // @name        SynLib
 // @namespace   Violentmonkey Scripts
 // @match       https://portal.librus.pl/rodzina/synergia/loguj*
-// @grant       none
+// @match       https://api.librus.pl/OAuth/Authorization*
+// @resource    login.html https://raw.githubusercontent.com/Ja-Tar/SynLib/main/login.html
+// @resource    SynLib_login.css https://raw.githubusercontent.com/Ja-Tar/SynLib/main/SynLib.login.css
+// @grant       GM.addStyle
+// @grant       GM.getResourceText
 // @run-at      document-end
 // @version     0.0.1
 // @author      JaTar
 // @description Teraz to wygląda! Poprawia wygląd Librusa.
 // ==/UserScript==
 
-setTimeout(() => {
+if (window.top !== window.self) {
+    GM.addStyle(GM.getResourceText('SynLib_login.css'));
+}
+else {
+    // Nasłuchiwanie na załadowanie się elementu iframe
+    document.getElementById('caLoginIframe').addEventListener('load', function () {
 
-// Funkcja do pobierania zawartości strony i przetwarzania jej
-fetch('https://raw.githubusercontent.com/Ja-Tar/SynLib/main/login.html')
-    .then(response => response.text())
-    .then(html => {
         // Pobierz zawartość body z pobranego pliku HTML
-        var importedBodyContent = html;
+        var importedBodyContent = GM.getResourceText('login.html');
 
         // Przeskanuj oryginalną zawartość strony i dodaj wszystkie skrypty na początek nowego body
         var originalScripts = document.querySelectorAll('script');
@@ -26,7 +31,7 @@ fetch('https://raw.githubusercontent.com/Ja-Tar/SynLib/main/login.html')
         });
 
         // Skopiuj zawartość elementu do zachowania
-        var elementDoZachowania = document.getElementById('synergiaLogin');
+        var elementDoZachowania = document.getElementById('caLoginIframe');
         var preservedContent = elementDoZachowania.cloneNode(true);
 
         // Wyczyść zawartość elementu body
@@ -35,5 +40,19 @@ fetch('https://raw.githubusercontent.com/Ja-Tar/SynLib/main/login.html')
         // Dodaj zachowane elementy oraz skrypty na początek nowego body
         document.body.innerHTML += scriptsToInject + importedBodyContent;
         document.getElementById('login').appendChild(preservedContent);
+
+        // Pobranie elementu iframe
+        var iframe = document.getElementById('caLoginIframe');
+
+        addStyleToFrame('body { background: #222; }', iframe);
     });
-}, 3000);
+
+    function addStyleToFrame(cssStr, frmNode) {
+        var D = frmNode.contentDocument;
+        var newNode = D.createElement('style');
+        newNode.textContent = cssStr;
+
+        var targ = D.getElementsByTagName('head')[0] || D.body || D.documentElement;
+        targ.appendChild(newNode);
+    }
+}
