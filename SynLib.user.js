984922fe3d2d5f96f64cc1545d66c20c105e8f77
Ja Tar <tarnogorskijan@gmail.com>
2024-03-03 17:24:39 +0100

    Aktualizacja SynLib do wersji 0.0.13, dodanie obiektu Strona, usunięcie zbędnego kodu

--- SynLib.user.js
+++ SynLib.user.js
@@ -1,11 +1,9 @@
 // ==UserScript==
 // @name        SynLib
 // @namespace   Violentmonkey Scripts
-// @version     0.0.12
+// @version     0.0.13
 // @author      JaTar
 // @description Teraz to wygląda! Poprawia wygląd Librusa.
-//
-// @require     https://github.com/Ja-Tar/SynLib/raw/main/Strona.js
 //
 // @updateURL   https://github.com/Ja-Tar/SynLib/raw/main/SynLib.user.js
 //
@@ -30,6 +28,78 @@
 // @run-at      document-end
 // ==/UserScript==
 
+// ==========
+// Obiekt Strona (zawiera wszystkie funkcje które są wywoływane na danej stronie)
+// ==========
+
+const Strona = {
+    Login() {
+        document.getElementById('caLoginIframe').addEventListener('load', function () {
+            // Pobierz zawartość body z pobranego pliku HTML
+
+            getFile('login.html').then(importedBodyContent => _changeBodyLogin(importedBodyContent));
+
+            function _changeBodyLogin(importedBodyContent) {
+                // Przeskanuj oryginalną zawartość strony i dodaj wszystkie skrypty na początek nowego body
+                var originalScripts = document.querySelectorAll('script');
+                var scriptsToInject = '';
+                originalScripts.forEach(script => {
+                    scriptsToInject += script.outerHTML;
+                });
+
+                // Skopiuj zawartość elementu do zachowania
+                var elementDoZachowania = document.getElementById('caLoginIframe');
+                var preservedContent = elementDoZachowania.cloneNode(true);
+
+                // Wyczyść zawartość elementu body
+                document.body.innerHTML = '';
+
+                // Dodaj zachowane elementy oraz skrypty na początek nowego body
+                document.body.innerHTML += scriptsToInject + importedBodyContent;
+                if (TrybJanosc !== true) {
+                    getFile('SynLib_main.css').then(
+                        stle => GM.addStyle(stle)
+                    );
+                }
+                document.getElementById('login').appendChild(preservedContent);
+            }
+        });
+    },
+    Index() {
+        // Usuń wszystkie elementy <style>
+        document.querySelectorAll('style').forEach(style => style.remove());
+
+        // Usuń wszystkie linki do arkuszy stylów
+        document.querySelectorAll('link[rel="stylesheet"]').forEach(link => link.remove());
+
+        getFile('SynLib_main.css').then(
+            stle => GM.addStyle(stle)
+        );
+        getFile('Iconoir.css').then(
+            stle => GM.addStyle(stle)
+        );
+
+        document.getElementById('cookieBox').remove();
+        document.getElementById('footer').remove();
+        document.getElementById('body').innerHTML = '';
+        document.getElementById('main-navigation-container').remove();
+        document.getElementById('user-section').remove();
+        getFile('ribbon.html').then(html => {
+            document.getElementById('top-banner-container').innerHTML = html;
+            connectRibbonButtons();
+        });
+
+        saveAfterLoginData();
+        
+        // Stwórz nowy element <div>
+        var centre = document.createElement('div');
+    }
+}
+
+// ==========
+// Skrypt
+// ==========
+
 // Skip home page
 if (location.hostname === 'portal.librus.pl' && location.pathname != "/rodzina/synergia/loguj") {
     location.replace("/rodzina/synergia/loguj");
@@ -37,7 +107,6 @@ if (location.hostname === 'portal.librus.pl' && location.pathname != "/rodzina/s
 
 var GMLoadedLevel;
 var TrybJanosc = false;
-var Strona = window.Strona;
 
 // Sprawdź czy wszystkie GM są dostępne
 if (GM.getResourceText && GM.addStyle && GM.xmlHttpRequest) {
@@ -71,6 +140,11 @@ else {
     }
 }
 
+// ==========
+// Funkcje
+// ==========
+
+// Pobierz zawartość url za pomocą GM.xmlHttpRequest
 async function GetXMLHttpRequest(url) {
     return new Promise((resolve, reject) => {
         GM.xmlHttpRequest({
@@ -86,6 +160,7 @@ async function GetXMLHttpRequest(url) {
     });
 }
 
+// Pobierz zawartość pliku za pomocą GM.getResourceText lub GM.xmlHttpRequest
 async function getFile(filename) {
     if (GMLoadedLevel === 1) {
         try {
@@ -100,6 +175,7 @@ async function getFile(filename) {
     }
 }
 
+// Pobieranie zawartości CSS i dodanie tagu <style>
 async function addTagStyle(gm_text) {
     try {
         const importedStyleContent = await getFile(gm_text, GMLoadedLevel);
@@ -110,6 +186,7 @@ async function addTagStyle(gm_text) {
     }
 }
 
+// Komunikacja z native API Librusa
 async function getDataFromLibrusAPI(endpoint) {
     const url = `https://synergia.librus.pl/gateway/api/2.0/${endpoint}`;
     const response = await fetch(url, {
@@ -134,6 +211,7 @@ async function getDataFromLibrusAPI(endpoint) {
     return json;
 }
 
+// Zapisz dane z API Librusa do sessionStorage
 async function saveAfterLoginData() {
     try {
         await getDataFromLibrusAPI('Me').then(data => {
@@ -145,7 +223,6 @@ async function saveAfterLoginData() {
         await getDataFromLibrusAPI('Classes').then(data => {
             sessionStorage.setItem('Classes', JSON.stringify(data['Class']));
         });
-        sessionStorage.setItem('SavedAfterLogin', true);
     } catch (error) {
         try {
             console.warn(error);
